f7aa98dd26679133f52eb2a5b8a385754e082d64
Sarin Khoonjun <61907135+EverytimeAomsin@users.noreply.github.com>
2023-06-12 02:13:24 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// ทำให้ไฟล์ภายในโฟลเดอร์ 'books' สามารถเข้าถึงได้ผ่านเส้นทาง '/img'
+app.use('/img/books', express.static(path.join(__dirname, 'img/books')));
+
+app.listen(6800, () => {
+  console.log('Server is running at http://localhost:3000');
+});
