b8ea3fddcde99dac1c2d469d2df6971dc54b02b4
Sarin Khoonjun <61907135+EverytimeAomsin@users.noreply.github.com>
2023-06-22 09:34:08 +0700

    เพิ่ม html /

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.get('/', function (req, res) {
+  res.send('DOH18 CDN file Service \n|||--||| Contact dev: 082-430-3880 or sarin542@hotmail.com');
+});
+
 // ทำให้ไฟล์ภายในโฟลเดอร์ 'books' สามารถเข้าถึงได้ผ่านเส้นทาง '/img'
 app.use('/img/books/', express.static(path.join(__dirname, 'img/books')));
+
+app.use('/video/drone', express.static(path.join(__dirname, 'video/drone')));
 
 
 app.listen(5900, () => {
